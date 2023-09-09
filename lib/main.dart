5df2482378898fe6764eede74d3a9f33934c87da
Zuhaib Shah <34958019+zaibugemi@users.noreply.github.com>
2023-09-09 17:20:01 +0500

    feat: show saved rates in the settings screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,28 +12,31 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Electricity Bill Calculator',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return ChangeNotifierProvider(
+      create: (context) => CategoriesState(),
+      child: MaterialApp(
+        title: 'Electricity Bill Calculator',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // TRY THIS: Try running your application with "flutter run". You'll see
+          // the application has a blue toolbar. Then, without quitting the app,
+          // try changing the seedColor in the colorScheme below to Colors.green
+          // and then invoke "hot reload" (save your changes or press the "hot
+          // reload" button in a Flutter-supported IDE, or press "r" if you used
+          // the command line to start the app).
+          //
+          // Notice that the counter didn't reset back to zero; the application
+          // state is not lost during the reload. To reset the state, use hot
+          // restart instead.
+          //
+          // This works for code too, not just values: Most code changes can be
+          // tested with just a hot reload.
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const MyHomePage(),
       ),
-      home: const MyHomePage(),
     );
   }
 }
@@ -94,16 +98,8 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class ElectricityBillCalculator extends StatefulWidget {
-  const ElectricityBillCalculator({Key? key}) : super(key: key);
-
-  @override
-  State<ElectricityBillCalculator> createState() =>
-      _ElectricityBillCalculatorState();
-}
-
-class _ElectricityBillCalculatorState extends State<ElectricityBillCalculator> {
-  final electricitySettings = ElectricitySettings(categories: {
+class CategoriesState extends ChangeNotifier {
+  var electricitySettings = ElectricitySettings(categories: {
     'Domestic': Category(name: 'Domestic', isFlatRate: false, rates: <Rate>[
       Rate(startUnits: 0, endUnits: 50, rate: 7.0),
       Rate(startUnits: 51, endUnits: 150, rate: 9.0),
@@ -115,41 +111,54 @@ class _ElectricityBillCalculatorState extends State<ElectricityBillCalculator> {
         isFlatRate: true,
         rates: <Rate>[Rate(startUnits: 0, endUnits: 99999, rate: 7.0)]),
   });
+}
+
+class ElectricityBillCalculator extends StatefulWidget {
+  const ElectricityBillCalculator({Key? key}) : super(key: key);
+
+  @override
+  State<ElectricityBillCalculator> createState() =>
+      _ElectricityBillCalculatorState();
+}
+
+class _ElectricityBillCalculatorState extends State<ElectricityBillCalculator> {
+  double billAmount = 0;
 
   final lastReadingController = TextEditingController();
   final newReadingController = TextEditingController();
   final categoryController = TextEditingController(text: 'Domestic');
 
-  double billAmount = 0;
-
-  double calculateBill() {
-    int lastReading = int.tryParse(lastReadingController.text) ?? 0;
-    int newReading = int.tryParse(newReadingController.text) ?? 0;
-    String selectedCategory = categoryController.text;
-    Category category = electricitySettings.categories[selectedCategory]!;
-    int unitsConsumed = newReading - lastReading;
-
-    if (category.isFlatRate) {
-      return unitsConsumed * category.rates.first.rate;
-    } else {
-      double totalBill = 0;
-      for (var rate in category.rates) {
-        if (unitsConsumed <= 0) {
-          break;
-        }
-        int unitsInCurrentRange = rate.endUnits - rate.startUnits + 1;
-        int unitsToConsider = unitsConsumed <= unitsInCurrentRange
-            ? unitsConsumed
-            : unitsInCurrentRange;
-        totalBill += unitsToConsider * rate.rate;
-        unitsConsumed -= unitsToConsider;
-      }
-      return totalBill;
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
+    var categoriesState = context.watch<CategoriesState>();
+    var electricitySettings = categoriesState.electricitySettings;
+
+    double calculateBill() {
+      int lastReading = int.tryParse(lastReadingController.text) ?? 0;
+      int newReading = int.tryParse(newReadingController.text) ?? 0;
+      String selectedCategory = categoryController.text;
+      Category category = electricitySettings.categories[selectedCategory]!;
+      int unitsConsumed = newReading - lastReading;
+
+      if (category.isFlatRate) {
+        return unitsConsumed * category.rates.first.rate;
+      } else {
+        double totalBill = 0;
+        for (var rate in category.rates) {
+          if (unitsConsumed <= 0) {
+            break;
+          }
+          int unitsInCurrentRange = rate.endUnits - rate.startUnits + 1;
+          int unitsToConsider = unitsConsumed <= unitsInCurrentRange
+              ? unitsConsumed
+              : unitsInCurrentRange;
+          totalBill += unitsToConsider * rate.rate;
+          unitsConsumed -= unitsToConsider;
+        }
+        return totalBill;
+      }
+    }
+
     return Padding(
       padding: const EdgeInsets.only(left: 10.0, right: 10.0),
       child: Center(
@@ -215,7 +224,80 @@ class SettingsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    var categoriesState = context.watch<CategoriesState>();
+    var electricityCategories = categoriesState.electricitySettings.categories;
+
+    return Column(
+      children: <Widget>[
+        Expanded(
+          child: ListView.builder(
+            itemCount: electricityCategories
+                .length, // Replace with your actual item count
+            itemBuilder: (BuildContext context, int index) {
+              // Build your list items here
+              var category = electricityCategories[
+                  electricityCategories.keys.elementAt(index)];
+              return ExpansionTile(
+                title: Text(category!.name),
+                children: <Widget>[
+                  DataTable(
+                    columns: const [
+                      DataColumn(
+                          label: Expanded(
+                        child: Text(
+                          'Units',
+                          style: TextStyle(fontStyle: FontStyle.italic),
+                        ),
+                      )),
+                      DataColumn(
+                          label: Expanded(
+                        child: Text(
+                          'Rate',
+                          style: TextStyle(fontStyle: FontStyle.italic),
+                        ),
+                      ))
+                    ],
+                    rows: category.rates.map((r) {
+                      return DataRow(cells: <DataCell>[
+                        DataCell(Text('${r.startUnits} - ${r.endUnits}')),
+                        DataCell(Text('${r.rate}'))
+                      ]);
+                    }).toList(),
+                  )
+                ],
+              );
+            },
+          ),
+        ),
+        Container(
+          height: 10,
+          color: Colors.white,
+        ),
+        // Fixed element at the bottom
+        Container(
+          color: Colors.blue, // Background color for the fixed element
+          padding: const EdgeInsets.all(16.0), // Adjust padding as needed
+          child: Row(
+            children: <Widget>[
+              const Text(
+                'Fixed Bottom Element',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18.0,
+                ),
+              ),
+              const Spacer(), // Add space to push content to the right
+              ElevatedButton(
+                onPressed: () {
+                  // Handle button press
+                },
+                child: const Text('Button'),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
 
